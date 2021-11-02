9d8ddd2b9e16b293a5c7d5b573fc00446acf6600
VictorAp06 <victor.p02@hotmail.com>
2021-11-02 01:37:52 -0300

    Inicio desenvolvimento rotas de autenticacao

--- resources/js/resources/login/loginComponent.vue
+++ resources/js/resources/login/loginComponent.vue
@@ -1,37 +1,48 @@
 <template>
-    <div id="login">
-        <div class="container">
-            <div id="login-row" class="row justify-content-center align-items-center">
-                <div id="login-column" class="col-md-6">
-                    <div id="login-box" class="col-md-12">
-                        <form id="login-form" class="form" action="" method="post">
-                            <h3 class="text-center text-info">Social Uniube</h3>
-                            <div class="form-group">
-                                <label for="username" class="text-info">Usuário:</label><br>
-                                <input type="text" name="username" id="username" class="form-control">
+        <div id="app">
+            <login-component>
+                <div class="container">
+                    <div id="login-row" class="row justify-content-center align-items-center">
+                        <div id="login-column" class="col-md-6">
+                            <div id="login-box" class="col-md-12">
+                                <form id="login-form" class="form" action="validate" method="post">
+                                    <h3 class="text-center text-info">Social Uniube</h3>
+                                    <div class="form-group">
+                                        <label for="username" class="text-info">Usuário:</label><br>
+                                        <input type="text" name="username" id="username" class="form-control">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="password" class="text-info">Senha:</label><br>
+                                        <input type="password" name="password" id="password" class="form-control">
+                                    </div>
+                                    <div class="form-group mt-2">
+                                        <input type="submit" name="submit" class="btn btn-info btn-md" value="Entrar">
+                                    </div>
+                                </form>
+                                <div id="register-link" class="text-right">
+                                    <a href="/cadastro" class="text-info">Registre-se</a>
+                                </div>
                             </div>
-                            <div class="form-group">
-                                <label for="password" class="text-info">Senha:</label><br>
-                                <input type="password" name="password" id="password" class="form-control">
-                            </div>
-                            <div class="form-group mt-2">
-                                <input type="submit" name="submit" class="btn btn-info btn-md" value="Entrar">
-                            </div>
-                        </form>
-                        <div id="register-link" class="text-right">
-                            <a href="/cadastro" class="text-info">Registre-se</a>
                         </div>
                     </div>
                 </div>
-            </div>
+            </login-component>
         </div>
-    </div>
 </template>
 
 <script>
 export default {
-    setup() {
-        
+    data() {
+        return {
+            form: {
+                segmento: '',
+                escolas: [],
+            },
+            multiselect_escola_length: 0,
+            segmentos_teste: this.segmentos,
+            escolas_mutable: this.escolas,
+            dmodal: new DModal(this.$modal)
+        }
     },
 }
 </script>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -10,7 +10,9 @@
     {{-- HTML element to mount the Vue application --}}
     <div id="app">
         {{-- the Vue component --}}
-        </loginComponent>
+        <login-component>
+
+        </login-component>
     </div>
     {{-- including the app.js file --}}
     <script src="{{ asset('js/app.js') }}"></script>
